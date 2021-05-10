5057d12dbe0969beb6e294a1638c589b9d1a2141
HermannJoel <hermannjoel.ngayap@yahoo.fr>
2021-05-10 17:34:02 +0200

    added projects 3

--- Inflation_Expectation/Fred_Expected_Inflation.R
+++ Inflation_Expectation/Fred_Expected_Inflation.R
@@ -294,12 +294,38 @@ regr.eval(test$Target, preds)
 library(usethis)
 use_git_config(user.name="HermannJoel", user.email="hermannjoel.ngayap@yahoo.fr")
 
-#Project 2
+#Project 2/ Telcom customers churn prediction: Project overview
+#Probleme statement:Customer churn, also known as customer retention, 
+# customer turnover, or customer defection, is the loss of clients or customers
+# Telephone service companies, Internet service providers, pay TV companies, insurance firms, 
+# and alarm monitoring services, often use customer attrition analysis and customer attrition 
+# rates as one of their key business metrics because the cost of retaining an existing customer 
+# is far less than acquiring a new one
+
+#Goal: Predict behavior to retain customers. 
+# The business can analyze all relevant customer data and develop focused customer retention programs.
+#Data:Each row represents a customer, each column contains customer’s attributes described on the column Metadata.
+#-Customers who left within the last month – the column is called Churn
+
+The raw data contains 7043 rows (customers) and 21 columns (features).
+
+The “Churn” column is our target.
 telcom <- read.csv(file.choose())
 str(telcom)
 summary(telcom)
 
+#Project 3 Direct Marketing
+#Problem statement: Targeting customers requires resources and cost. the company want 
+#only those who are most likely to be interested in a offer. Targeting only the potential customers 
+#will protect the brand image and save resources and cost by not spamming those who doesn't whant to be contacted
+#Goal: The goil is to predict if a given client will subscribe to a term deposit 
+#Data: This data is related wit direct marketing campaigns of a portuguese institution
+bank_data <- read.csv(file.choose(), sep = ";")
+str(bank_data)
+summary(bank_data)
+head(bank_data)
 
+table(bank_data$subscribed)
 
 
 

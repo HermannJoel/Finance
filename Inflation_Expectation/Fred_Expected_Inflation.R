47da89d834f932d70ea20d6c18ad89d67ca17939
HermannJoel <hermannjoel.ngayap@yahoo.fr>
2021-05-10 16:02:00 +0200

    added project 3

--- Inflation_Expectation/Fred_Expected_Inflation.R
+++ Inflation_Expectation/Fred_Expected_Inflation.R
@@ -299,6 +299,16 @@ telcom <- read.csv(file.choose())
 str(telcom)
 summary(telcom)
 
+#Project 3 Direct Marketing
+#Problem statement: Targeting customers requires resources and cost. the company want 
+#only those who are most likely to be interested in a offer. Targeting only the potential customers 
+#will protect the brand image and save resources and cost by not spamming those who doesn't whant to be contacted
+#Goal: The goil is to predict if a given client will subscribe to a term deposit 
+#Data: This data is related wit direct marketing campaigns of a portuguese institution
+bank_data <- read.csv(file.choose(), sep = ";")
+str(bank_data)
+summary(bank_data)
+head(bank_data)
 
 
 
